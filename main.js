d6f0b75a9d7792e43a62ac09f0e44a71470d637a
mtrojas <tererojas.i@gmail.com>
2018-06-26 21:06:26 -0500

    tue

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ var ctx = canvas.getContext('2d');
 var interval;
 var frames = 0;
 //here we save an array of colors that we can assign randomly as a text to the first word and as a font color to the second word
-var colors = ["black", "yellow", "orange", "red", "blue", "purple"]
-var random1 = Math.floor(Math.random() * colors.length);
-var random2 = Math.floor(Math.random() * colors.length);
+var colors = ["black", "yellow", "orange", "red", "blue", "purple"];
 //we can save the images that we are going to use here
 var images = {
   bg: "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
@@ -30,24 +28,12 @@ class Board {
   }
   draw(){ //we draw the image outside the constructor, but inside our class, we include the timer and the scores 
     ctx.drawImage(this.image, this.x, this.y, this.width,this.height);
-    //score player 1
-    ctx.font = '48px Avenir';
-    ctx.fillStyle = 'white';
-    ctx.fillText(this.scorePlayer1, 900, 50);
-    //score player 2
-    ctx.font = '48px Avenir';
-    ctx.fillStyle = 'white';
-    ctx.fillText(this.scorePlayer2, 950, 50);
-    //timer
-    // ctx.font = '60px Avenir';
-    // ctx.fillStyle = 'black';
-    // ctx.fillText("00", 200, 200);
   }
 }
 
 class Text {
-  constructor(y, color, text) {
-    this.x = (canvas.width / 3) - 80;
+  constructor(x, y, color, text) {
+    this.x = x;
     this.y = y;
     this.width = 200;
     this.height = 100;
@@ -60,6 +46,108 @@ class Text {
     ctx.fillText(this.text, this.x, this.y); 
   } 
 }
+
+//3.Instances
+var board = new Board();
+var text1 = new Text(canvas.width/2 - 100, 300, "black", generateColor());
+var text2 = new Text(canvas.width/2 - 100, 400, generateColor(), generateColor());
+var timer = new Text(canvas.width/2 - 100, 500, "black", "00");
+var score1 = new Text(900, 60, "white", 0);
+var score2 = new Text(980, 60, "white", 0);
+
+//4.Main f(x)s
+function update() {
+  if (frames <= 30) {
+    timer.text = frames;  
+    frames++;
+  } else {
+    return;
+  } 
+  ctx.clearRect(0, 0, canvas.width, canvas.height) //siempre borra primero
+  board.draw();
+  text1.draw();
+  text2.draw();
+  timer.draw();
+  score1.draw();
+  score2.draw();
+}
+
+function start() {
+  interval = setInterval(update, 1000);
+  //console.log(interval)
+}
+
+//5.Aux f(s)s
+function generateColor() {
+  return colors[ Math.floor(Math.random() * colors.length)];
+}
+// function generateColor2() {
+//   return colors[ Math.floor(Math.random() * colors.length)];
+// }
+
+function checkIfCorrect(answer) {
+  if(text1.text === text2.color && answer === true) {
+    score1.color = "green";
+    score1.text++;
+    keepPlaying();  
+  } else if(text1.text === text2.color && answer === false) {
+    score1.color = "red";
+    keepPlaying(); 
+  } else if(text1.text !== text2.color && answer === true) {
+    score1.color = "red";
+    keepPlaying();
+  } else if(text1.text !== text2.color && answer === false) {
+    score1.color = "green";
+    score1.text++;
+    keepPlaying(); 
+  }
+}
+
+function keepPlaying() {
+  text1.text = generateColor();
+  text2.color = generateColor();
+  text2.text = generateColor();
+} 
+
+function game2() {
+
+}
+
+
+//6.Listeners 
+addEventListener('keydown', function(e) {
+  switch(e.keyCode) {
+    case 32: 
+    start();
+    document.getElementById('start').innerHTML = 'Player 2';
+    break;
+    case 37: //arrow left no 
+    checkIfCorrect(false);
+    break;
+    case 39: //arrow right yes 
+    checkIfCorrect(true);
+    break;
+  }
+});
+
+
+//levels?
+//As soon as the start button or the enter key are pressed, the game begins
+//That means the set of cards are displayed and the explaining widgets that give the user a hint of how to play 
+//prompt are your ready? yes no
+//if yes start the timer
+//case 1:
+//if the first text coincides with the color of the second text the user should click the yes button or the right arrow
+//being that correct, a point is added to the score board, there is a unicorn or a fish or a turtle, and the next set of cards is 
+//presented
+//case 2:
+//if the text does not match the color of the second text, there is no point added and it gets a cross or a down thumb
+//when the timer finishes, is congratulated and the score it got, invites the second player to come in
+//the same happens to the second player, when he finishes the board announces the winner 
+
+//30 seconds it's the first setTimeout
+
+
 
 // class Card {
 //   constructor(y, color) {
@@ -78,75 +166,15 @@ class Text {
 //   } 
 // }
 
+// document.getElementById("start").addEventListener('click', function(){
+//   //7.Start the game
+//   start();
+// });
 
-//3.Instances
-var board = new Board();
-var text1 = new Text(300, "black", generateColor1());
-var text2 = new Text(400, generateColor2(), generateColor2());
-var timer = new Text(500, "black", "00");
-//var score1 = new Text(20, );
+// document.getElementById("yes").addEventListener('click', function(e){
+// console.log(e);
+// });
 
-//4.Main f(x)s
-function update() {
-  //frames++;
-  ctx.clearRect(0, 0, canvas.width, canvas.height) //siempre borra primero
-  board.draw();
-  text1.draw();
-  text2.draw();
-  timer.draw();
-}
-
-function start() {
-  interval = setInterval(update, 1000/60);
-}
-
-//5.Aux f(s)s
-function generateColor1() {
-  return colors[random1];
-}
-function generateColor2() {
-  return colors[random2];
-}
-
-//6.Listeners 
-document.getElementById("start").addEventListener('click', function(){
-  //7.Start the game
-  start();
-});
-
-document.getElementById("yes").addEventListener('click', function(e){
-console.log(e);
-});
-
-document.getElementById("no").addEventListener('click', function(e){
-  console.log(e);
-});
-
-addEventListener('keydown', function(e) {
-  switch(e.keyCode) {
-    case 37:
-    console.log(e.keyCode);  
-    //no arraw left
-      break;
-    case 39:
-    console.log(e.keyCode); 
-    //yes arrow right
-      break;
-  }
-});
-
-//levels?
-//As soon as the start button or the enter key are pressed, the game begins
-//That means the set of cards are displayed and the explaining widgets that give the user a hint of how to play 
-//prompt are your ready? yes no
-//if yes start the timer
-//case 1:
-//if the first text coincides with the color of the second text the user should click the yes button or the right arrow
-//being that correct, a point is added to the score board, there is a unicorn or a fish or a turtle, and the next set of cards is 
-//presented
-//case 2:
-//if the text does not match the color of the second text, there is no point added and it gets a cross or a down thumb
-//when the timer finishes, is congratulated and the score it got, invites the second player to come in
-//the same happens to the second player, when he finishes the board announces the winner 
-
-//30 seconds it's the first setTimeout
+// document.getElementById("no").addEventListener('click', function(e){
+//   console.log(e);
+// });
