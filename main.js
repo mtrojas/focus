699ffb679aecadbc5e6179cea420455fa822dc94
mtrojas <tererojas.i@gmail.com>
2018-06-28 20:07:20 -0500

    thu night

--- main.js
+++ main.js
@@ -119,8 +119,6 @@ var text1 = new Text(canvas.width/2 - 90, 210, 'black', generateColor());
 var text2 = new Text(canvas.width/2 - 90, 400, generateColor(), generateColor());
 var unicorn = new Player(845, 60, images.unicorn, 50, 50);
 var star = new Player(945, 65, images.star, 40, 40);
-// var meaning = new Explain(100, 250, 'black', 'meaning>');
-// var textColor = new Explain(100, 380, 'black', 'text color>');
 
 user = score1;
 
@@ -143,8 +141,6 @@ function update() {
   score2.draw();
   unicorn.draw();
   star.draw();
-  //meaning.draw();
-  //textColor.draw();
   afterGame();
 }
 
@@ -152,7 +148,7 @@ function start() {
   interval = setInterval(update, 1000);
 }
 
-//5.Aux f(s)s
+//5.Aux f(x)s
 function generateColor() {
   return colors[ Math.floor(Math.random() * colors.length)];
 }
@@ -192,25 +188,20 @@ function player2() {
   user = score2;
   keepPlaying();
   start();
-  
 }
 
 function afterGame() {
-  console.log('antes de decidir')
   if(user == score2 && frames === 30) {
     score2.color = "white";
-    console.log('ya hay ganador')
     if(score1.text > score2.text){
       document.getElementById('winner').innerHTML = 'UNICORN WINS!';
-      document.getElementById('keep-practice').innerHTML = 'Keep practicing';
-      console.log('user unicorn wins')
+      document.getElementById('keep-practice').innerHTML = 'Keep practicing ; )';
     } else if(score1.text < score2.text){
       document.getElementById('winner').innerHTML = 'STAR WINS!';
-      document.getElementById('keep-practice').innerHTML = 'Keep practicing';
-      console.log('user star wins')
+      document.getElementById('keep-practice').innerHTML = 'Keep practicing ; )';
     } else if(score1.text === score2.text) {
       document.getElementById('winner').innerHTML = 'GREAT GAME!';
-      document.getElementById('keep-practice').innerHTML = 'Keep practicing';
+      document.getElementById('keep-practice').innerHTML = 'Keep practicing : )';
     }
   }
 }
@@ -227,96 +218,12 @@ document.getElementById("player-2").addEventListener('click', function(){
 });
 
 document.getElementById("yes").addEventListener('click', function(){
-//console.log(e);
   checkIfCorrect(user,true);
 });
 
 document.getElementById("no").addEventListener('click', function(){
-  //console.log(e);
   checkIfCorrect(user, false);
 });
 
-// addEventListener('keydown', function(e) {
-//   switch(e.keyCode) {
-//     case 32: 
-//     start();
-//     //document.getElementById('start').innerHTML = 'Player 2';
-//     break;
-//     case 37: //arrow left no 
-//     checkIfCorrect(user, false);
-//     break;
-//     case 39: //arrow right yes 
-//     checkIfCorrect(user,true);
-//     break;
-//     case 78: 
-//     player2();
-//   }
-// });
-
-//levels?
-//Errores: dejar las instruccions al usuario para el final
-//challenges, incluir las instrucciones, incorporar al jugador 2, 
-//As soon as the space bar is pressed, the game begins
-//That means the set of cards are displayed and the explaining widgets that give the user a hint of how to play 
-//prompt are your ready? yes no
-//if yes start the timer
-//case 1:
-//if the first text coincides with the color of the second text the user should click the yes button or the right arrow
-//being that correct, a point is added to the score board, there is a unicorn or a fish or a turtle, and the next set of cards is 
-//presented
-//case 2:
-//if the text does not match the color of the second text, there is no point added and it gets a cross or a down thumb
-//when the timer finishes, is congratulated and the score it got, invites the second player to come in
-//the same happens to the second player, when he finishes the board announces the winner 
-
-//30 seconds it's the first setTimeout
 
 
-
-
-
-//Does the meaning match the text color?
-//Meaning - text color
-
-// document.getElementById("start").addEventListener('click', function(){
-//   //7.Start the game
-//   start();
-// });
-
-// document.getElementById("yes").addEventListener('click', function(e){
-// console.log(e);
-// });
-
-// document.getElementById("no").addEventListener('click', function(e){
-//   console.log(e);
-// });
-
-
-// class Text {
-//   constructor(x, y, color, text) {
-//     this.x = x;
-//     this.y = y;
-//     this.width = 200;
-//     this.height = 100;
-//     this.color = color;
-//     this.text = text;
-//   }
-//   draw() {   
-//     ctx.fillStyle = this.color;
-//     ctx.font = '60px Avenir';
-//     var metrics = ctx.measureText(this.text);
-//     var width = metrics.width; 
-//     ctx.fillText(this.text, canvas.width/2 - width/2, this.y); 
-//   } 
-// }
-
-// class Explain extends Score {
-//   constructor(x, y, color, text) {
-//     super(x, y, color, text);
-//   }
-//   draw() {   
-//     ctx.fillStyle = this.color;
-//     ctx.font = 'italic 30px Avenir';
-//     ctx.fillText(this.text, this.x, this.y); 
-//   } 
-// }
